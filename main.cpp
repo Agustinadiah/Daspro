f12cc345865575cf1d93da1ec2f51aab69166849
Agustinadiah <112885568+Agustinadiah@users.noreply.github.com>
2022-09-12 21:27:33 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Agustina Diah Kusuma Dewi" << endl;
-    cout << "A11.2022.14122" << endl;
-    cout << "Perum PKS Cindelaras RT 04 RW 06, Kaliwungu Kendal" << endl;
-    cout << "SMK TEXMACO SEMARANG" << endl;
+    cout << "Hello world!" << endl;
+    /*
+    printf("Test");
+    integer
+    jgdwhje
+    kjedheu
+    */
+
+    int u = 18;
+    float t = 160.5;
+    string ub = ("xxl");
+
+    cout << "Umur Saya : " << u;
+    cout << "\nTinggi saya : " << t;
+    cout << "\nUkuran baju saya : " << ub;
+
     return 0;
 }
